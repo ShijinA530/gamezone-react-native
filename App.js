181259006ec0521be998fc1c8d2de769b5884b2a
ShijinA530 <shijinasasi835@gmail.com>
2024-08-15 12:37:23 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from "./screens/home";
+import ReviewDetails from './screens/reviewDetails';
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+const Stack = createNativeStackNavigator()
+
+
+
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const  [loaded, error] = useFonts({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  })
+ 
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={ReviewDetails} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/about.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { globalStyles } from '../styles/global'
+
+export default function About() {
+  return (
+    <View style={globalStyles.container}>
+      <Text>About Screen</Text>
+    </View>
+  )
+}
+
